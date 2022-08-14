f5d0ef2820f9af3c3ed964558f7af13cbe6d01c5
Caroline <carolfranca0310@gmail.com>
2022-08-14 20:13:04 -0300

    :tada: Criação de model da entidade Reservas

---
+++ src/model/ReservasModel.js
@@ -0,0 +1,11 @@
+class ReservasModel{
+    constructor(idCliente, qtdDias, checkIn, checkOut, idQuarto){
+        this.idCliente = idCliente
+        this.qtdDias = qtdDias
+        this.checkIn = checkIn
+        this.checkOut = checkOut
+        this.idQuarto = idQuarto
+    }
+}
+
+export default ReservasModel
