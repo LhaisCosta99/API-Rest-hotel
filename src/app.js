cb83320d351b38ffaaba49f9130735f5adc5db0c
Lhais <lhaisferreirada@gmail.com>
2022-08-14 00:42:36 -0300

    adição de cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import HospedesController from "./controllers/HospedesController.js";
 import HospedesDAO from "./DAO/HospedesDAO.js";
+import cors from "cors"
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
