4a6d410dbc83932395cb82063129e1b94f29b866
Lhais <lhaisferreirada@gmail.com>
2022-09-17 03:16:41 -0300

    service de login

--- src/controllers/HospedesController.js
+++ src/controllers/HospedesController.js
@@ -1,5 +1,6 @@
 import HospedesRepository from "../repository/HospedesRepository.js"
-import ValidacoesHospede from "../services/hospedesService.js"
+import HospedeService from "../services/HospedeService.js"
+import ValidacoesHospede from "../services/hospedesValidacoesService.js"
 
 class HospedesController{
     static rotas(app){
@@ -28,18 +29,41 @@ class HospedesController{
         app.post("/hospedes", async (req, res) => {
             try {
                 ValidacoesHospede.validaHospede(req.body.nome, req.body.cpf, req.body.email, req.body.telefone)
-
+                
                 const hospede = req.body
-
+                
                 const inserir = await HospedesRepository.criarHospede(hospede)
                 
                 res.status(201).json(inserir)
-
+                
             } catch (erro) {
                 res.status(400).json({message: erro.message})
             }
         })
-    
+
+        app.post('/hospedes/login', async (req, res)=>{
+            try {
+                if(!req.body || req.body.senha === "" || req.body.email === "" || !req.body.email || !req.body.senha){
+                    throw new Error("Input incompleto, favor passar email e senha no objeto.")
+                }
+                const payload = req.body
+
+                const response = await HospedeService.login(payload)
+                res.status(200).json(response)
+            } catch (e) {
+
+                if(e.message == "Input incompleto, favor passar email e senha no objeto."){
+                    res.status(400).json({status: 400, message: e.message})
+                }
+
+                if(e.message == "Usuario com senha incorreta."){
+                    res.status(403).json({status: 403, message: e.message})
+                }
+
+                res.status(404).json({status: 404, message: "Usuário não encontrado."})
+            }
+        })
+        
         app.patch("/hospedes/:id", async (req, res)=>{
             const id = req.params.id
             const body = Object.entries(req.body)
